8d63839c0422328749d745ea6e72edb52feea51e
Aiden-Python <rahulraman1604@gmail.com>
2020-05-18 22:55:18 +0530

    updated v_0.0.3

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'megamind',         # How you named your package folder (MyLib)
   packages = ['megamind'],   # Chose the same as "name"
-  version = '0.0.2',      # Start with a small number and increase it with every change you make
+  version = '0.0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Deep Learning Framework',   # Give a short description about your library
   author = 'Rahul Raman',                   # Type in your name
   author_email = 'rahulraman1604@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Aiden-Python/Megamind.git',   # Provide either the link to your github or to your 
-  download_url = 'https://github.com/Aiden-Python/Megamind/archive/v_0.0.2.tar.gz',    # I explain this later 
+  download_url = 'https://github.com/Aiden-Python/Megamind/archive/v_0.0.3.tar.gz',    # I explain this later 
   keywords = ['deep learning', 'neural network', 'from scratch'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
